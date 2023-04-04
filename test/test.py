48718adda127dd2071c460667a1954af0d0a9e47
Yoek7 <129858449+Yoek7@users.noreply.github.com>
2023-04-04 16:21:03 +0200

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,21 +1,23 @@
 import wiringpi as wp
 import time
 
-# Setup WiringPi library and pins
+# Setup WiringPi library
 wp.wiringPiSetup()
-PIN = 18  # use GPIO pin 18 (BCM numbering)
-wp.softPwmCreate(PIN, 0, 100)  # initialize PWM output
+
+# Configure SPI interface
+SPI_CHANNEL = 0
+SPI_SPEED = 800000  # Hz
+wp.wiringPiSPISetup(SPI_CHANNEL, SPI_SPEED)
 
 # Define function to set LED color
 def set_color(r, g, b):
-    wp.softPwmWrite(PIN, r)  # set red value
-    wp.softPwmWrite(PIN + 1, g)  # set green value
-    wp.softPwmWrite(PIN + 2, b)  # set blue value
+    data = [0x00, b, g, r]
+    wp.wiringPiSPIDataRW(SPI_CHANNEL, bytes(data))
 
 # Flash white light for 5 seconds
 start_time = time.time()
 while time.time() - start_time < 5:
-    set_color(100, 100, 100)  # set white color
+    set_color(255, 255, 255)  # set white color
     time.sleep(0.5)
     set_color(0, 0, 0)  # turn off
     time.sleep(0.5)
