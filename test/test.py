f79af1ecf319bb073bb435a21abce83bee1efa26
Yoek7 <129858449+Yoek7@users.noreply.github.com>
2023-04-05 15:59:07 +0200

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,27 +1,53 @@
 import time
-from periphery import GPIO
+import RPi.GPIO as GPIO
 
-# Replace with the appropriate GPIO pin number
-gpio_pin_number = 13
+# Define the GPIO pin number that the SK6812 LED is connected to
+LED_PIN = 18
 
-# Initialize GPIO as output
-gpio_out = GPIO(gpio_pin_number, "out")
+# Define the number of LEDs in the strip
+NUM_LEDS = 1
 
-# Set the time delay (in seconds) for toggling the GPIO pin
-time_delay = 0.001
+# Initialize the GPIO library
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
-try:
-    while True:
-        # Toggle the GPIO pin to create a square wave
-        gpio_out.write(True)
-        time.sleep(time_delay)
-        gpio_out.write(False)
-        time.sleep(time_delay)
+# Set up the LED pin as an output
+GPIO.setup(LED_PIN, GPIO.OUT)
 
-except KeyboardInterrupt:
-    # Turn off the buzzer when the script is interrupted
-    gpio_out.write(False)
+# Define a function to set the color of the LED
+def set_color(red, green, blue):
+    # Convert the RGB values to a single 24-bit value
+    color = (red << 16) | (green << 8) | blue
 
-finally:
-    # Close the GPIO pin
-    gpio_out.close()
+    # Send the color data to the LED
+    for i in range(NUM_LEDS):
+        for j in range(23, -1, -1):
+            bit = (color >> j) & 1
+            if bit:
+                GPIO.output(LED_PIN, GPIO.HIGH)
+                time.sleep(0.0008)
+                GPIO.output(LED_PIN, GPIO.LOW)
+                time.sleep(0.0004)
+            else:
+                GPIO.output(LED_PIN, GPIO.HIGH)
+                time.sleep(0.0004)
+                GPIO.output(LED_PIN, GPIO.LOW)
+                time.sleep(0.0008)
+
+# Set the LED color to red
+set_color(255, 0, 0)
+
+# Wait for 1 second
+time.sleep(1)
+
+# Set the LED color to green
+set_color(0, 255, 0)
+
+# Wait for 1 second
+time.sleep(1)
+
+# Set the LED color to blue
+set_color(0, 0, 255)
+
+# Clean up the GPIO library
+GPIO.cleanup()
